0962b1a0118122688ea3010fc3d544b9e6cea6f1
Алтынай Абдыкалыкова <altynai@Altynajs-MacBook-Air.local>
2023-09-22 02:05:25 -0700

    completed assignment 1

---
+++ src/AssignCh1.java
@@ -0,0 +1,21 @@
+public class AssignCh1 {
+    public static void main(String[] args) {
+        printA();
+        printB();
+        printC();
+        printB();
+        printA();
+    }
+
+    public static void printA() {
+        System.out.print("A ");
+    }
+
+    public static void printB() {
+        System.out.print("B ");
+    }
+
+    public static void printC() {
+        System.out.print("C ");
+    }
+}
